f182fe9439c05b174e0ac95b458eb481215d7671
Stephen Wetzel <stephen.wetzel1@gmail.com>
2017-04-04 19:22:53 -0400

    Use env file

--- pi_client.rb
+++ pi_client.rb
@@ -1,11 +1,14 @@
 require 'action_cable_client'
+require 'dotenv/load'
 
 # https://github.com/jwhitehorn/pi_piper
 # https://github.com/ClockVapor/rpi_gpio
 
 EventMachine.run do
-  uri = "wss://admin:asdf@pi-controller.herokuapp.com/cable/"
-  # uri = "ws://admin:asdf@localhost:3000/cable/"
+  username = ENV['HTTP_AUTH_USER']
+  password = ENV['HTTP_AUTH_PASS']
+  uri = "wss://#{username}:#{password}@pi-controller.herokuapp.com/cable/"
+  # uri = "ws://#{username}:#{password}@localhost:3000/cable/"
   puts "URL: #{uri}"
   client = ActionCableClient.new(uri, 'EventChannel')
   # the connected callback is required, as it triggers
@@ -22,6 +25,8 @@ EventMachine.run do
     state_code = message['message']['state_code']
     event_log_id = message['message']['event_log_id']
 
+    puts "Power ON"
+
     response_dt = Time.current
     client.perform('response', { response_dt: response_dt, response: "OK", event_log_id: event_log_id })
   end
@@ -32,9 +37,20 @@ EventMachine.run do
 
   client.disconnected do
     puts "DISCONNECTED"
+    puts Time.current
+    # refire connect
   end
 
   puts "After received"
+
+  # while true
+  # EM.next_tick do
+  #   EventMachine::Synchrony.sleep(1)
+  #   request_dt = Time.current
+  #   puts "#{client.subscribed?} #{request_dt}"
+  #   # client.perform('ping', { request_dt: request_dt })
+  # end
+
   # adds to a queue that is purged upon receiving of
   # a ping from the server
   client.perform('response', { message: 'hello from amc' })
