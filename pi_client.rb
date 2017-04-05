d9a9fa0da2d48323d464e9f0f69c6519b3ee8b3d
Stephen Wetzel <stephen.wetzel1@gmail.com>
2017-04-04 20:08:24 -0400

    Check for disconnect and kill job if so

--- pi_client.rb
+++ pi_client.rb
@@ -3,55 +3,62 @@ require 'dotenv/load'
 
 # https://github.com/jwhitehorn/pi_piper
 # https://github.com/ClockVapor/rpi_gpio
+# cron job reboots pi nightly
+# cron job checks most recent ping every minute, pkill job and relaunch
+
 
 EventMachine.run do
+  latest_ping_timestamp = 0
+  THIS_DEVICE_GUID = "0f0f4e02-ed27-448c-bfdb-3fcc6e5239d1"
   username = ENV['HTTP_AUTH_USER']
   password = ENV['HTTP_AUTH_PASS']
   uri = "wss://#{username}:#{password}@pi-controller.herokuapp.com/cable/"
-  # uri = "ws://#{username}:#{password}@localhost:3000/cable/"
+  uri = "ws://#{username}:#{password}@localhost:3000/cable/"
   puts "URL: #{uri}"
   client = ActionCableClient.new(uri, 'EventChannel')
-  # the connected callback is required, as it triggers
-  # the actual subscribing to the channel but it can just be
-  # client.connected {}
-  client.connected { puts 'successfully connected.' }
+  client.connected { puts 'successfully connected.' } # Required to trigger subscription
 
   # called whenever a message is received from the server
-  client.received do | message |
-    puts client.subscribed?
-    puts message
-    device_guid = message['message']['device_guid']
-    event_code = message['message']['event_code']
-    state_code = message['message']['state_code']
-    event_log_id = message['message']['event_log_id']
+  client.received(false) do | message |
+    if message['type'] && message['type'] == 'ping'
+      puts "#{Time.current} PING"
+      latest_ping_timestamp = message['message']
+    else
+      puts message
+      device_guid = message['message']['device_guid']
+      event_code = message['message']['event_code']
+      state_code = message['message']['state_code']
+      event_log_id = message['message']['event_log_id']
 
-    puts "Power ON"
-
-    response_dt = Time.current
-    client.perform('response', { response_dt: response_dt, response: "OK", event_log_id: event_log_id })
+      if THIS_DEVICE_GUID == device_guid && state_code == 'ON'
+        puts "Power ON"
+        response_dt = Time.current
+        client.perform('response', { response_dt: response_dt, response: "OK", event_log_id: event_log_id, device_guid: THIS_DEVICE_GUID })
+      end
+    end
   end
 
   client.subscribed do
-    puts "Subscribed hook"
+    puts "#{Time.current} Subscribed"
   end
 
   client.disconnected do
-    puts "DISCONNECTED"
-    puts Time.current
+    puts "#{Time.current} DISCONNECTED"
     # refire connect
   end
 
-  puts "After received"
+  client.errored do | message |
+    puts "#{Time.current} ERROR"
+    puts message
+  end
 
-  # while true
-  # EM.next_tick do
-  #   EventMachine::Synchrony.sleep(1)
-  #   request_dt = Time.current
-  #   puts "#{client.subscribed?} #{request_dt}"
-  #   # client.perform('ping', { request_dt: request_dt })
-  # end
-
-  # adds to a queue that is purged upon receiving of
-  # a ping from the server
-  client.perform('response', { message: 'hello from amc' })
+  EM.add_periodic_timer(10) do
+    if (Time.current.to_i - latest_ping_timestamp > 10) || !client.subscribed?
+      puts "#{Time.current} No recent pings"
+      abort("Disconnected, exiting...")
+      client.instance_variable_set("@_websocket_client", EventMachine::WebSocketClient.connect(client._uri))
+      client.connected { puts "Attempting to reconnect..." }
+      client.subscribed { puts "Subscribed" }
+    end
+  end
 end
